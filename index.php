202da4c403285ccc51ba3ece1dc03b98ea2dd5ba
Ryan Becker <ryan@nationalparkreservations.com>
2014-06-24 09:53:59 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  	<title>This is my main index page</title>
+    <meta name="viewport" content="width=device-width">
+  </head>
+  
+  <body>
+  	<p>Just a sample page to get things rolling.</p>
+  </body>
+</html>
