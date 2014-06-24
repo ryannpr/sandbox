9ce27430b25fa8b91f904eb1b098e2ad0083c71e
Ryan Becker <ryan@nationalparkreservations.com>
2014-06-24 10:20:15 -0600

    added index page and stylesheet

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   	<title>This is my main index page</title>
     <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="style1.css">
   </head>
   
   <body>
